6b01f0a97db9b898d21bde97e5eeceb42b4b4848
Bluesbaka <badunnells@gmail.com>
2019-06-10 19:03:33 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles, only:[:new, :create, :index, :update, :show]
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
